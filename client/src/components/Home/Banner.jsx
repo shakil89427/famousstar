8dbb21039b38d0005c61e9ee5d1831f5a4668e7c
shakil89427 <shakilahmed89427@gmail.com>
2023-01-11 11:49:35 +0600

    banner btn added

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -8,58 +8,68 @@ const Banner = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="grid grid-cols-12 rounded-xl overflow-hidden max-w-[1100px] mx-auto">
-      <div className=" bg-black text-white px-5 py-7 sm:p-10 md:p-5 lg:p-10 col-span-12 md:col-span-7 flex items-center justify-start">
-        <div className="w-full">
-          <p
-            style={{ lineHeight: "120%" }}
-            className="font-bold text-2xl sm:text-3xl md:text-2xl lg:text-4xl xl:text-5xl max-w-[15ch]"
-          >
-            Instagram Users Analytics Tool
-          </p>
-          <p className="font-medium sm:text-lg md:text-base lg:text-lg xl:text-xl max-w-[25ch] mt-5">
-            Analyze Detailed view of Instagram Profiles
-          </p>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              setUrl(e.target[0].value);
-              navigate("/result");
-            }}
-          >
-            <label
-              htmlFor="url"
-              className="flex items-center mt-5 rounded-lg overflow-hidden h-11 sm:h-12 md:h-11 lg:h-12 bg-[#444444] border"
+    <div className="max-w-[1100px] mx-auto">
+      <div className="w-full grid grid-cols-12 rounded-xl overflow-hidden">
+        <div className=" bg-black text-white px-5 py-7 sm:p-10 md:p-5 lg:p-10 col-span-12 md:col-span-7 flex items-center justify-start">
+          <div className="w-full">
+            <p
+              style={{ lineHeight: "120%" }}
+              className="font-bold text-2xl sm:text-3xl md:text-2xl lg:text-4xl xl:text-5xl max-w-[15ch]"
             >
-              <div className="h-full aspect-square flex items-center justify-center bg-black border-r">
-                <img
-                  src={instagram}
-                  alt=""
-                  className="rounded-lg w-1/2 aspect-square"
+              Instagram Users Analytics Tool
+            </p>
+            <p className="font-medium sm:text-lg md:text-base lg:text-lg xl:text-xl max-w-[25ch] mt-5">
+              Analyze Detailed view of Instagram Profiles
+            </p>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                setUrl(e.target[0].value);
+                navigate("/result");
+              }}
+            >
+              <label
+                htmlFor="url"
+                className="flex items-center mt-5 rounded-lg overflow-hidden h-11 sm:h-12 md:h-11 lg:h-12 bg-[#444444] border"
+              >
+                <div className="h-full aspect-square flex items-center justify-center bg-black border-r">
+                  <img
+                    src={instagram}
+                    alt=""
+                    className="rounded-lg w-1/2 aspect-square"
+                  />
+                </div>
+                <input
+                  id="url"
+                  type="url"
+                  placeholder="https://instagram.com/therock"
+                  required
+                  className="grow border-0 outline-none px-2 bg-transparent h-full"
                 />
-              </div>
-              <input
-                id="url"
-                type="url"
-                placeholder="https://instagram.com/therock"
-                required
-                className="grow border-0 outline-none px-2 bg-transparent h-full"
-              />
-            </label>
-            <button
-              type="submit"
-              className="bg-white text-black w-40 h-11 sm:h-12 md:h-11 lg:h-12 rounded-lg font-semibold mt-5"
-            >
-              View Result
-            </button>
-          </form>
+              </label>
+              <button
+                type="submit"
+                className="bg-white text-black w-40 h-11 sm:h-12 md:h-11 lg:h-12 rounded-lg font-semibold mt-5"
+              >
+                View Result
+              </button>
+            </form>
+          </div>
         </div>
+        <img
+          src={bannerbg}
+          alt=""
+          className="hidden md:block md:col-span-5 h-full w-full object-cover object-center"
+        />
       </div>
-      <img
-        src={bannerbg}
-        alt=""
-        className="hidden md:block md:col-span-5 h-full w-full object-cover object-center"
-      />
+      <p
+        style={{
+          backgroundImage: "linear-gradient(20deg, #FA8F21, #AE3497)",
+        }}
+        className="w-full max-w-[380px] text-center mx-auto mt-12 py-4 rounded-lg text-white text-lg font-semibold select-none"
+      >
+        User Instagram Analytics Result
+      </p>
     </div>
   );
 };
--- client/src/components/Home/ResultBottom.jsx
+++ client/src/components/Home/ResultBottom.jsx
@@ -1,9 +1,54 @@
+import blackbg from "../../assets/blackbg.png";
+import getdetailedbg from "../../assets/getdetailedbg.png";
+import map from "../../assets/map.png";
+import girlbg from "../../assets/girlbg.png";
+
 const ResultBottom = () => {
-    return (
-        <div>
-            dbvdfbb
-        </div>
-    );
+  return (
+    <div className="max-w-[1100px] mx-auto">
+      <div
+        style={{ backgroundImage: `url(${blackbg})` }}
+        className="mt-12 text-white p-5 lg:p-10 xl:p-12 rounded-xl flex flex-col items-start justify-start gap-7 lg:text-lg font-medium bg-cover bg-center bg-no-repeat"
+      >
+        <p className="text-2xl lg:text-3xl xl:text-4xl font-semibold">
+          How does it work
+        </p>
+        <p>
+          With this website, you can easily fetch Instagram users analytics
+          easily Just follow these steps:
+        </p>
+        <li className="list-disc">Copy the unique Url of the desired user.</li>
+        <li className="list-disc">
+          Paste this Url into the field at the top of the page.
+        </li>
+        <li className="list-disc">
+          After Click on Get Result Button, it will list all the details on the
+          bottom page.
+        </li>
+        <li className="list-disc">
+          Analyze Users Data like Followers, likes, Posts data, engagement and
+          much more.
+        </li>
+      </div>
+      <div className="mt-12">
+        <p className="text-center text-xl lg:text-2xl xl:text-3xl font-semibold">
+          Get Detailed View Analytics
+        </p>
+        <img
+          src={getdetailedbg}
+          alt=""
+          className="mt-7 w-full max-w-[400px] block mx-auto"
+        />
+      </div>
+      <img src={map} alt="" className="mt-16 w-full" />
+      <div className="mt-16">
+        <p className="text-center text-xl lg:text-2xl xl:text-3xl font-semibold">
+          Instagram Post Analytics
+        </p>
+        <img src={girlbg} alt="" className="mt-7 w-full" />
+      </div>
+    </div>
+  );
 };
 
 export default ResultBottom;
